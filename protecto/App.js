5110fe6266e3fd0b90ae5ebeddd35fc3e1bfb335
unknown <velascoalex121@gmail.com>
2020-06-30 12:07:04 -1200

    30/06/2020

--- protecto/App.js
+++ protecto/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { styleSheet, View, Text, StatusAreaView, StatusBar, SafeAreaView } from 'react-native';
+import { styleSheet, View, Text, StatusAreaView, StatusBar, SafeAreaView, YellowBox } from 'react-native';
 import color from './src/utils/colors'
 import colors from './src/utils/colors';
 import Form from './src/components/Form'
+YellowBox.ignoreWarnings(["Pcker has been  extracted"])
 export default function app() {
   return (
     //renderizar para que nuestra apliacacion devuelva un solo valor con una etiqueta vacia
     <>
-      < StatusBar barStyle="light-content" />
+     
       <SafeAreaView style={styles.safeArea}>
+        <View style={styles.backgroud}/>
         <Text style={styles.tittleApp}>cotizador de prestamos </Text>
-      <Form/>
+        <Form />
       </SafeAreaView>
 
       <View>
@@ -21,7 +23,7 @@ export default function app() {
       <View>
         <Text>
           footer
-  </Text>
+        </Text>
 
       </View>
     </>
@@ -30,10 +32,20 @@ export default function app() {
 const styles = styleSheet({
   safeArea: {
     backgroundColor: colors.PRIMARY_COLOR,
-    height: 200,
-    borderBottomLeftRadius: 30,
-    borderBottomRighRadius: 30,
+    height: -90,
     alingnItems: "center"
+  },
+  backgroud:{
+backgroundColor:colores.PRIMARY_COLOR,
+height:200,
+width:'100%',
+borderBottomLeftRadius: 30,
+borderBottomRighRadius: 30,
+position:"absolute",
+zIndex:-1,
+
+
+
   },
   tittleApp: {
     fontSize: 25,
--- protecto/src/components/Form.js
+++ protecto/src/components/Form.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import RNPickerSelect from 'react-native-picker-select';
 import {
     StyleSheet,
     TextInput,
     View
 } from 'react-native'
 import color from '../utils/colors';
+import colors from '../utils/colors';
 export default function Form() {
     return (
-        <View>
-            <View>
-                <TextInput placeholder="cantidad a pedir" />
-                <TextInput placeholder="Porcentaje" />
+        <View style={styles.ViewForm}>
+            <View style={styles.inputView}>
+                <TextInput placeholder="cantidad a pedir" 
+                keyboardType="numeric"
+                style={styles.input}
+                />
+                <TextInput placeholder="Porcentaje" 
+                keyboardType="numeric"
+                style={[styles.input,styles.inputPorcentaje]}
+                />
 
             </View>
-
+            <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Football', value: 'football' },
+                { label: 'Baseball', value: 'baseball' },
+                { label: 'Hockey', value: 'hockey' },
+            ]}
+        />
         </View>
 
 
@@ -21,3 +36,56 @@ export default function Form() {
 
     )
 }
+const styles= StylesSheet.create({
+ViewForm:{
+position:"adsolute",
+bottom:0,
+width:"85%",
+paddingHorizontal:50,
+backgroundColor:color.PRYMARY_COLOR_DARK,
+borderRadius:30,
+height:200,
+justifyContent:'center'
+
+},
+inputView:{
+flexDirection:'row'
+
+},
+input:{
+
+    heiht: 50,
+    backgroundColor:"#fff",
+    borderWidth:1,
+    borderColor:color.PRYMARY_COLOR_DARK,
+    borderRadius:5,
+    width:'60%',
+    marginLeft:-5,
+    marginRight:5,
+    marginButtom:10,
+    color:"#000",
+    paddingHorizontal:20
+},
+inputPorcentaje:{
+width:"40%",
+marginLeft:5,
+
+}
+
+});
+const piketSelectStyle=StyleSheet.create({
+    inputIOS:{
+        fontSize:16,
+        paddingVertical:12,
+        paddingHorizontal:10,
+        borderWidth:1,
+        borderColor:'grey',
+        borderRadius:4,
+        color:'black',
+        paddingColor:"#fff",
+        marginLeft:-5,
+        marginRight:-5.
+    }
+    
+    
+    });
